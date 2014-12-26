efde3b5a3f0e9c749108b093d95547d6acf6ab0a
maxarnheiter <maxarnheiter@gmail.com>
2014-12-26 11:52:22 -0500

    Partial Testing
    Weird stuff going on. Ignore.

---
+++ Assets/Scripts/Components/AnimTester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimTester : MonoBehaviour {
+
+	public Sprite leftFoot;
+	public Sprite rightFoot;
+	
+	SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+	
+		this.renderer = this.gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
