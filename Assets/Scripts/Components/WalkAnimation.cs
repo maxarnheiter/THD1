f755e371bcf69fcb13baa51e1b0ccef935c85406
maxarnheiter <maxarnheiter@gmail.com>
2014-12-26 12:10:57 -0500

    More Partial
    Think I'm scrapping this branch.

---
+++ Assets/Scripts/Components/WalkAnimation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimTester : MonoBehaviour {
+
+	public Sprite standing_north;
+	public Sprite walking_north_left;
+	public Sprite walking_north_right;
+	
+	public Sprite standing_south;
+	public Sprite walking_south_left;
+	public Sprite walking_south_right;
+	
+	public Sprite standing_east;
+	public Sprite walking_east_left;
+	public Sprite walking_east_right;
+	
+	public Sprite standing_west;
+	public Sprite walking_west_left;
+	public Sprite walking_west_right;
+	
+	public float speed = 1f;
+	public float elapsedTime = 0f;
+	
+	SpriteRenderer renderer;
+
+
+	void Start () 
+	{
+		renderer = this.gameObject.GetComponent<SpriteRenderer>();
+		
+	}
+	
+
+	void FixedUpdate () 
+	{
+			
+			elapsedTime += Time.deltaTime;
+			
+			if(elapsedTime >= speed)
+			{
+				elapsedTime = 0;
+				
+				
+			}
+			
+	}
+}
