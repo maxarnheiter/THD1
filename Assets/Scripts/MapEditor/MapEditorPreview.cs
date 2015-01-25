62b0c2dc36937265847891158967a5ee29537b70
maxarnheiter <maxarnheiter@gmail.com>
2015-01-24 19:07:13 -0500

    UML cleanup
    Cleaned up a lot of the relationships in the uml diagrams. Also did some
    minor cleanup to the map editor preview class.

--- Assets/Scripts/MapEditor/MapEditorPreview.cs
+++ Assets/Scripts/MapEditor/MapEditorPreview.cs
@@ -2,7 +2,8 @@
 using UnityEditor;
 using System.Collections;
 
-public static class MapEditorPreview {
+public static class MapEditorPreview 
+{
 
 	static GameObject _preview;
 	static SpriteRenderer _previewRenderer;
@@ -12,13 +13,15 @@ public static class MapEditorPreview {
 	static Color _addColor = new Color(0f, 1f, 0f, _previewTransparency);
 	static Color _removeColor = new Color(1f, 0f, 0f, _previewTransparency);
 	
-	static string floorSortingLayerName {
+	static string floorSortingLayerName 
+	{
 		get { return ("Floor " + MapEditor.floor); }
 	}
 
 	static int previewSortingOrder = 32000;
 
-	static void CreatePreviewObject() {
+	static void CreatePreviewObject() 
+	{
 		
 		while(GameObject.Find("Preview Object") != null)
 			Object.DestroyImmediate(GameObject.Find("Preview Object"));
@@ -42,11 +45,13 @@ public static class MapEditorPreview {
 		_previewRenderer.sortingOrder = previewSortingOrder;
 	}
 	
-	public static void OnActionChanged() {
+	public static void OnActionChanged() 
+	{
 		DisableIfNoPrefab();
 		
 		if(_preview) 
-		switch(MapEditor.action) {
+		switch(MapEditor.action) 
+		{
 			case ClickAction.Add : _previewRenderer.color = _addColor; 
 			break;
 			case ClickAction.Remove : _previewRenderer.color = _removeColor;
@@ -54,7 +59,8 @@ public static class MapEditorPreview {
 		}
 	}
 	
-	public static void OnPositionChanged() {
+	public static void OnPositionChanged() 
+	{
 		DisableIfNoPrefab();
 		
 		if(!_preview)
@@ -64,15 +70,18 @@ public static class MapEditorPreview {
 		SceneView.RepaintAll();
 	}
 	
-	public static void OnFloorChanged() {
+	public static void OnFloorChanged() 
+	{
 		DisableIfNoPrefab();
 		
 		if(_preview)
 			_previewRenderer.sortingLayerName = floorSortingLayerName;
 	}
 	
-	static void DisableIfNoPrefab() {
-		if(_previewRenderer != null) {
+	static void DisableIfNoPrefab() 
+	{
+		if(_previewRenderer != null) 
+		{
 			if(PrefabManager.current == null)
 				_previewRenderer.enabled = false;
 			else
